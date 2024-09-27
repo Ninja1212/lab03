58ca5248b88771e1a58bf9e30750ab01f558c68b
Ninja1212 <nima.nazarina@gmail.com>
2024-09-26 19:58:28 -0700

    2024210084_NIMANAZARI_LAB03_CPSC110

--- hello.py
+++ hello.py
@@ -1,1 +1,19 @@
-# Answer Problem 1 here
+''' Answer Problem 1 here '''
+from cs110 import expect, summarize
+
+def add_hello_world(s: str)-> str:
+    """
+    Purpose: returns a string with "Hello world! " at the front of it
+    Examples:
+    add_hello_world("Good Morning") # "Hello world! Good Morning"
+    add_hello_world("Python Programming") # "Hello world! Python Programming"
+    add_hello_world("Welcome to coding") # "Hello world! Welcome to coding"
+    """
+    
+    return "Hello world! " + s     #stub
+
+# testing
+expect(add_hello_world("Good Morning"), equals = "Hello world! Good Morning")
+expect(add_hello_world("Python Programming"), equals = "Hello world! Python Programming")
+expect(add_hello_world("Welcome to coding"), equals = "Hello world! Welcome to coding")
+summarize()
